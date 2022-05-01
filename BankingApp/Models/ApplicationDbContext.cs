1254aa4f055bdc09de70e98b07a58b4630e05b98
chinezprost <47869879+chinezprost@users.noreply.github.com>
2022-05-01 19:35:32 +0300

    Initital git commit

---
+++ BankingApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankingApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<User> User { get; set; }
+    }
+    
+    
+
+}
---
+++ BankingApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public float Money { get; set; }
+        
+        public bool IsDeveloper { get; set; }
+        
+    }
+}
